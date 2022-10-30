3e790fc9e41add0bbf0c30eb55d1c5a3abcd1509
joe p <joep1231026@gmail.com>
2022-10-30 11:50:23 -0500

    streamlit form input df debug 2

--- source/predict.py
+++ source/predict.py
@@ -101,10 +101,10 @@ races = ("White only",
          "Hispanic"
          )
 
-educags = ("Have not graduated high school",
+educags = ("Did not graduate high school",
            "Graduated high school",
-           "Some college",
-           "Graduated college"
+           "Attended college or technical school",
+           "Graduated college or technical school"
            )
 
 incomgs = ("Less than $15,000",
@@ -117,14 +117,13 @@ incomgs = ("Less than $15,000",
 
 smoker3s = ("Everyday smoker",
             "Smoke less than every day",
-            "Former smoker"
-            "Never smoker")
+            "Former smoker",
+            "Never smoked")
 
 drnkany5s = ("Yes",
              "No")
 
-rfbing5s = ("Yes",
-            "No")
+rfbing5s = ("No", "Yes")
 
 rfdrhv7s = ("No",
             "Yes")
@@ -133,7 +132,10 @@ pneumo3s = ("Yes",
             "No")
 
 rfseat3s = ("Always",
-            "Not always")
+            "Nearly always",
+            "Sometimes",
+            "Seldom",
+            "Never wear a seatbelt")
 
 drnkdrvs = ("Yes",
             "No")
@@ -152,12 +154,12 @@ rfpsa23s = ("Yes",
 
 crcrec1s = ("Yes",
             "Yes but not within time",
-            "Never")
+            "Never had any of the recommended CRC tests")
 
 aidtst4s = ("Yes",
             "No")
 
-persdoc2s = ("Yes",
+persdoc2s = ("Yes, only one",
              "More than one",
              "No")
 
@@ -262,34 +264,67 @@ def show_predict_page():
 	denvst3 = st.radio("Over the past year, have you visited the dentist?", denvst3s)
 	new_entry.iloc[0]._DENVST3 = denvst3s.index(denvst3) + 1
 	
-	st.write(f"variable: {denvst3} \n"
-	         f"df: {new_entry.iloc[0]._DENVST3}")
+	race = st.radio("With which race(s) do you identify?", races)
+	new_entry.iloc[0]._RACE = races.index(race) + 1
+	
+	educag = st.radio("What is your highest level of education?", educags)
+	new_entry.iloc[0]._EDUCAG = educags.index(educag) + 1
+	
+	incomg = st.radio("What is your annual houshold income from all sources?", incomgs)
+	new_entry.iloc[0]._INCOMG = incomgs.index(incomg) + 1
+	
+	smoker3 = st.radio("Do you smoke cigarettes?", smoker3s)
+	new_entry.iloc[0]._SMOKER3 = smoker3s.index(smoker3) + 1
+	
+	drnkany5 = st.radio("During the past month, have you had at least one drink of alcohol? ", drnkany5s)
+	new_entry.iloc[0].DRNKANY5 = drnkany5s.index(drnkany5) + 1
+	
+	rfbing5 = st.radio("During the past month, have you had at least one binge-drinking occasion (five or more drinks for males, or four or more drinks for females)?", rfbing5s)
+	new_entry.iloc[0]._RFBING5 = rfbing5s.index(rfbing5) + 1
+	
+	rfdrhv7 = st.radio("Are you a heavy drinker (14 or more drinks per week for males, or 7 or more drinks per week for females)?", rfdrhv7s)
+	new_entry.iloc[0]._RFDRHV7 = rfdrhv7s.index(rfdrhv7) + 1
+	
+	pneumo3 = st.radio("Have you ever has a pneumonia vaccination?", pneumo3s)
+	new_entry.iloc[0]._PNEUMO3 = pneumo3s.index(pneumo3) + 1
+	
+	rfseat3 = st.radio("How often do you wear a seatbelt when in a vehicle?", rfseat3s)
+	new_entry.iloc[0]._RFSEAT3 = 1 if rfseat3s.index(rfseat3) < 1 else 2
+	
+	drnkdrv = st.radio("Have you ever driven at least once when perhaps you have had too much to drink?", drnkdrvs)
+	new_entry.iloc[0]._DRNKDRV = drnkdrvs.index(drnkdrv) + 1
+	
+	rfmam22 = st.radio("Have you had a mammogram in the past 2 years?", rfmam22s)
+	new_entry.iloc[0]._RFMAM22 = rfmam22s.index(rfmam22) + 1
+	
+	flshot7 = st.radio("Have you had a flu shot within the past year?", flshot7s)
+	new_entry.iloc[0]._FLSHOT7 = flshot7s.index(flshot7) + 1
+	
+	rfpap35 = st.radio("Have you had a Pap test in the past 3 years?", rfpap35s)
+	new_entry.iloc[0]._RFPAP35 = rfpap35s.index(rfpap35) + 1
+	
+	rfpsa23 = st.radio("Have you had a PSA (prostate specific antigen) test in the past 2 years?", rfpsa23s)
+	new_entry.iloc[0]._RFPSA23 = rfpsa23s.index(rfpsa23) + 1
+	
+	crcrec1 = st.radio("Have you full met the USPSTF recommendations for colorectal cancer (CRC) screening (this includes a sigmoidoscopy within the past 10 years, a blood stool test within the past year, a stool DNA test within the past 3 years, or a colonoscopy within the past 10 years)?", crcrec1s)
+	new_entry.iloc[0]._CRCREC1 = crcrec1s.index(crcrec1) + 1
+	
+	aidtst4 = st.radio("Have you ever been tested for HIV?", aidtst4s)
+	new_entry.iloc[0]._AIDTST4 = aidtst4s.index(aidtst4) + 1
+	
+	persdoc2 = st.radio("Do you have one person you think of as your personal doctor or health care provider?", persdoc2s)
+	new_entry.iloc[0].PERSDOC2 = persdoc2s.index(persdoc2) + 1
+	
+	chcscncr = st.radio("Have you ever been told that you had skin cancer?", chcscncrs)
+	new_entry.iloc[0].CHCSCNCR = chcscncrs.index(chcscncr) + 1
+	
+	chcocncr = st.radio("Have you ever been told that you had any other types of cancer?", chcocncrs)
+	new_entry.iloc[0].CHCOCNCR = chcocncrs.index(chcocncr) + 1
+	
+	st.write(f"variable: {chcocncr} \n"
+	         f"df: {new_entry.iloc[0].CHCOCNCR}")
 	
 	
-	# new_entry.iloc[0]._RACE = st.selectbox("With which race(s) do you identify?", races)
-	# new_entry.iloc[0]._EDUCAG = st.radio("What is your highest level of education?", educags)
-	# new_entry.iloc[0]._INCOMG = st.radio("What is your annual houshold income from all sources?", incomgs)
-	# new_entry.iloc[0]._SMOKER3 = st.radio("Do you smoke cigarettes?", smoker3s)
-	# new_entry.iloc[0].DRNKANY5 = st.radio("During the past month, have you had at least one drink of alcohol? ", drnkany5s)
-	# new_entry.iloc[0]._RFBING5 = st.radio("During the past month, have you had at least one binge-drinking occasion"
-	#                    "(five or more drinks for males, or four or more drinks for females)?", rfbing5s)
-	# new_entry.iloc[0]._RFDRHV7 = st.radio("Are you a heavy drinker (14 or more drinks per week for males, or 7 or more drinks per week for"
-	#                    " females)?", rfdrhv7s)
-	# new_entry.iloc[0]._PNEUMO3 = st.radio("Have you ever has a pneumonia vaccination?", pneumo3s)
-	# new_entry.iloc[0]._RFSEAT3 = st.radio("Do you always wear a seatbelt when in a vehicle?", rfseat3s)
-	# new_entry.iloc[0]._DRNKDRV = st.radio("Do you always wear a seatbelt when in a vehicle?", drnkdrvs)
-	# new_entry.iloc[0]._DRNKDRV = st.radio("Have you ever driven at least once when perhaps you have had too much to drink?", drnkdrvs)
-	# new_entry.iloc[0]._RFMAM22 = st.radio("Have you had a mammogram in the past 2 years?", rfmam22s)
-	# new_entry.iloc[0]._FLSHOT7 = st.radio("Have you had a flu shot within the past year?", flshot7s)
-	# new_entry.iloc[0]._RFPAP35 = st.radio("Have you had a Pap test in the past 3 years?", rfpap35s)
-	# new_entry.iloc[0]._RFPSA23 = st.radio("Have you had a PSA (prostate specific antigen) test in the past 2 years?", rfpsa23s)
-	# new_entry.iloc[0]._CRCREC1 = st.radio("Have you full met the USPSTF recommendations for colorectal cancer screening "
-	#                    "(this includes a sigmoidoscopy within the past 10 years, a blood stool test within the past year,"
-	#                    "a stool DNA test within the past 3 years, or a colonoscopy within the past 10 years)?", crcrec1s)
-	# new_entry.iloc[0]._AIDTST4 = st.radio("Have you ever been tested for HIV?", aidtst4s)
-	# new_entry.iloc[0].PERSDOC2 = st.radio("Do you have one person you think of as your personal doctor or health care provider?", persdoc2s)
-	# new_entry.iloc[0].CHCSCNCR = st.radio("Have you ever been told that you had skin cancer?", chcscncrs)
-	# new_entry.iloc[0].CHCOCNCR = st.radio("Have you ever been told that you had any other types of cancer?", chcocncrs)
 	# new_entry.iloc[0].CHCCOPD2 = st.radio("Have you ever been told that you had chronic obstructive pulmonary disease, C.O.P.D.,"
 	#                     " emphysema or chronic bronchitis?", chccopd2s)
 	# new_entry.iloc[0].QSTLANG = st.radio("What language are you using to complete this questionnaire?", qstlangs)
